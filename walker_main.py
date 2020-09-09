a831712b0aeedd35b64d7c126a8b5737e0fa8592
corcor27 <cory13579@googlemail.com>
2020-09-09 17:25:17 +0100

    Update walker_main.py

--- walker_main.py
+++ walker_main.py
@@ -32,7 +32,7 @@ dis = UTILS.estimate_distance(threashold_image, 200)
 
 
 Vc = UTILS.threashold_volume(threashold_image)
-time = UTILS.time_exponential_function(t0, V0, alpha0, Vc) + 2
+time = UTILS.time_exponential_function(t0, V0, alpha0, Vc) + 4
 scale= int(round(dis/time))
 delay_start = 10 * scale
 scaled_time = (time * scale)
@@ -51,7 +51,7 @@ for t in range(delay_start, scaled_time):
         output = "/home/a.cot12/modeling/test/%s.png" %(epoch_time)
         plt.imsave(output, container_array, cmap = 'gray')
 	
-    if (np.sum(container_array)/Vc) >= 0.90:
+    if (np.sum(container_array)/Vc) >= 0.98:
         break
 
 output = "/home/a.cot12/modeling/test/final.png" 
