3a43f6b961dcf44115355a8122a66fa8b9fd8ee2
corcor27 <cory13579@googlemail.com>
2020-09-10 14:30:50 +0100

    Update walker_growth.py

--- walker_growth.py
+++ walker_growth.py
@@ -19,10 +19,7 @@ def RUN_RANDOM_GROWTH(container_array, walker_array, threashold_array, height, w
                 container_array[new_z, new_x] += 1
             else:
                 walker_array[0, walker],walker_array[1, walker]  = new_z, new_x
-                if threashold_array[new_z, new_x] == 0:
-                    ran_val = random.randint(0,walker_array.shape[1]-1)
-                    walker_array = walker_array[:, ran_val]
-            
+                
                 
     else:
                 
