e309c9f6c5e6a62ef14dcf82f0ed449146ac8bb1
corcor27 <cory13579@googlemail.com>
2020-09-11 12:09:40 +0100

    Update walker_growth.py

--- walker_growth.py
+++ walker_growth.py
@@ -21,7 +21,7 @@ def RUN_RANDOM_GROWTH(container_array, walker_array, threashold_array, height, w
                 
                 
     else:
-                
+        print(RW)     
         new_walkers = UTILS.new_growth_position_2(container_array, GV, walker_array)
         #container_array, new_walkers = UTILS.brightest_growth(threashold_array, container_array, GV)
         print(new_walkers)
