5f651d3763fe2239d7f76bf7587f151115765647
corcor27 <cory13579@googlemail.com>
2020-09-17 18:57:31 +0100

    Update utils.pyx

--- utils.pyx
+++ utils.pyx
@@ -6,7 +6,7 @@ from scipy.ndimage import gaussian_filter
 from datetime import datetime
 import random
 import cython
-cimport numpy as np
+cimport cython
 
 def RUN_RANDOM_GROWTH(container_array, walker_array, threashold_array, height, width, GV, batch_size):
     
