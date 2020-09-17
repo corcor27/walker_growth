456c38b536f63ff8afcd2a2a086b78cee2b87671
corcor27 <cory13579@googlemail.com>
2020-09-17 19:38:53 +0100

    Update cython_run.sh

--- cython_run.sh
+++ cython_run.sh
@@ -1,1 +1,9 @@
+#!/bin/bash --login
+#$ -cwd
+#SBATCH --job-name=grow_test
+#SBATCH --out=grow_test.out.%J
+#SBATCH --err=grow_test.err.%J
+#SBATCH --mem-per-cpu=4000
+#SBATCH --ntasks=1
 
+python walker_main.py
